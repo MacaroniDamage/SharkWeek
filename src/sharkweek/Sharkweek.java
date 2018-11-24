be6656cd073866cf571eec37d87414292bce8e16
Daniel <daniel.tohmas@hotmail.com>
2018-11-24 14:07:49 +0100

    Changed project structure

---
+++ src/sharkweek/Sharkweek.java
@@ -0,0 +1,11 @@
+package sharkweek;
+
+public class Sharkweek {
+
+	public static void main(String[] args) {
+		System.out.println("Nicht das Semicolon vergessen!");
+		System.out.println("Hello World");
+
+	}
+
+}
