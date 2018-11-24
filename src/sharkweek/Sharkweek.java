ab64b686c007834ccb8faeb283fa0df5c1db195a
Daniel <daniel.tohmas@hotmail.com>
2018-11-24 17:09:14 +0100

    Created Commands

--- src/sharkweek/Sharkweek.java
+++ src/sharkweek/Sharkweek.java
@@ -3,9 +3,42 @@ package sharkweek;
 public class Sharkweek {
 
 	public static void main(String[] args) {
-		System.out.println("Nicht das Semicolon vergessen!");
-		System.out.println("Hello World");
-
+		int zyklus = 0;
+		
+		if(args.length > 0) {
+			if(args[0].equals("show"))
+			{
+				System.out.println(args.length);
+				if(args.length == 1)
+				{
+					System.out.println("Der Kalender wird angezeigt");
+					System.out.println(args.length);
+				}
+				else if(args.length > 2)
+				{	
+					if(args[1].equals("prognosis"))
+					{
+						if(args.length == 2) 
+						{
+							System.out.println("Der Kalender mit den Zyklen wird angezeigt!");
+						}
+						else if(args.length == 3) 
+						{
+							//Intervall vom String in ein Integer konvertiert
+							zyklus = Integer.parseInt(args[2]);
+							//Gibt den gespeicherten Zyklus aus
+							System.out.println("Ein neuer Zyklus wurde definiert!");
+							System.out.println("Intervall: " + zyklus);
+						}
+					}
+				}
+			}//END OF ARGUMENT: SHOW
+			if(args[1].equals("setperiod"))
+			{
+				
+			}
+		}//END OF ARGUMENTS
+		
 	}
 
 }
---
+++ src/test/Test.java
@@ -0,0 +1,19 @@
+package test;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Test {
+	  public static void main(String[] args) {
+	    String pattern = "MM-dd-yyyy";
+	    SimpleDateFormat format = new SimpleDateFormat(pattern);
+	    try {
+	      Date date = format.parse("12/31-2006");
+	      System.out.println(date);
+	    } catch (ParseException e) {
+	      System.out.println("Entspricht nicht dem Format");
+	    }
+	    // formatting
+	    System.out.println(format.format(new Date()));
+	  }
+}
